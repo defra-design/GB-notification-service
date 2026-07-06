a79a15f2469038b3571463304eec7d0cb465384b
MrMister-88 <michael.doran@equalexperts.com>
2026-07-06 16:42:03 +0100

    new dashboard

--- app/data/dashboard-notifications.js
+++ app/data/dashboard-notifications.js
@@ -1,24 +1,293 @@
-const origins = ['France', 'Germany', 'Ireland', 'Netherlands', 'Spain', 'Poland', 'Denmark', 'Belgium']
-const statusOptions = ['Draft', 'Submitted', 'In progress']
-const commoditySets = [
-  '010410, 010420',
-  '0102',
-  '01061900',
-  '0101'
-]
-
-const notifications = Array.from({ length: 32 }, (_, index) => {
-  const number = index + 1
-
-  return {
-    reference: number === 1 ? 'GBN-AG-26-7K8M2P' : `GBN-AG-26-${String(number).padStart(6, '0')}`,
-    commodities: commoditySets[index % commoditySets.length],
-    statusText: number === 1 ? 'Draft' : statusOptions[index % statusOptions.length],
-    origin: origins[index % origins.length],
+const notifications = [
+  {
+    reference: 'GBN-AG-26-7K8M2P',
+    commodities: '010410, 010420',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'France',
     arrivalDate: '8 March 2026',
-    viewHref: number === 1 ? '/notification-hub' : '#'
+    viewHref: '/notification-hub'
+  },
+  {
+    reference: 'GBN-AG-26-B3N9QW',
+    commodities: '0102',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Germany',
+    arrivalDate: '12 March 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-C4R2TX',
+    commodities: '0101',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Ireland',
+    arrivalDate: '15 March 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-D5M7HK',
+    commodities: '0103',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Netherlands',
+    arrivalDate: '18 March 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-E6P1VN',
+    commodities: '01061900',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Spain',
+    arrivalDate: '21 March 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-F8L4JD',
+    commodities: '010511, 010512',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Poland',
+    arrivalDate: '24 March 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-G2W9RC',
+    commodities: '010614',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Denmark',
+    arrivalDate: '27 March 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-H7Y3MB',
+    commodities: '010410',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Belgium',
+    arrivalDate: '2 April 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-J1K6PF',
+    commodities: '0102, 0103',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'France',
+    arrivalDate: '5 April 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-K9D2SL',
+    commodities: '010420',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Germany',
+    arrivalDate: '9 April 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-L4H8WQ',
+    commodities: '0101',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Ireland',
+    arrivalDate: '11 April 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-M3T5NZ',
+    commodities: '010511',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Netherlands',
+    arrivalDate: '14 April 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-N6V1GC',
+    commodities: '01061900',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Spain',
+    arrivalDate: '17 April 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-P2X7RA',
+    commodities: '010614',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Poland',
+    arrivalDate: '20 April 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-Q8B4UE',
+    commodities: '010410, 010420',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Denmark',
+    arrivalDate: '23 April 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-R5C9HM',
+    commodities: '0102',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Belgium',
+    arrivalDate: '26 April 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-S1F3KT',
+    commodities: '0103',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'France',
+    arrivalDate: '29 April 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-T7J2WD',
+    commodities: '0101, 0102',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Germany',
+    arrivalDate: '3 May 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-U4L6YP',
+    commodities: '010512',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Ireland',
+    arrivalDate: '6 May 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-V9N8QJ',
+    commodities: '01061900',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Netherlands',
+    arrivalDate: '9 May 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-W2P5MC',
+    commodities: '010614',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Spain',
+    arrivalDate: '12 May 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-X6R1HB',
+    commodities: '010410',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Poland',
+    arrivalDate: '15 May 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-Y3S7LF',
+    commodities: '01064100',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Denmark',
+    arrivalDate: '18 May 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-Z8T4NG',
+    commodities: '0102',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Belgium',
+    arrivalDate: '21 May 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-A5U9VK',
+    commodities: '0101',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'France',
+    arrivalDate: '24 May 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-B1W2XD',
+    commodities: '0103, 010614',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Germany',
+    arrivalDate: '27 May 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-C7Y6PM',
+    commodities: '010512',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Ireland',
+    arrivalDate: '30 May 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-D4Z3QS',
+    commodities: '01061900',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Netherlands',
+    arrivalDate: '2 June 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-E9A8RT',
+    commodities: '010410, 010420',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Spain',
+    arrivalDate: '5 June 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-F2B5UW',
+    commodities: '010614',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Poland',
+    arrivalDate: '8 June 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-G6C1VX',
+    commodities: '0102',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Denmark',
+    arrivalDate: '11 June 2026',
+    viewHref: '#'
+  },
+  {
+    reference: 'GBN-AG-26-H3D7WY',
+    commodities: '0101, 0103',
+    statusText: 'Draft',
+    statusTagClass: 'govuk-tag--blue',
+    origin: 'Belgium',
+    arrivalDate: '14 June 2026',
+    viewHref: '#'
   }
-})
+]
 
 module.exports = {
   pageSize: 8,
